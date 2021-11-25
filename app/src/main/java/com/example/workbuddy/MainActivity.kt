2257c2b9caad33e8099ec513720796e1730c11d2
Lennart Möller <kontakt@lennartmoeller.com>
2021-11-25 13:01:14 +0100

    Add activity list with examples

--- app/src/main/java/com/example/workbuddy/MainActivity.kt
+++ app/src/main/java/com/example/workbuddy/MainActivity.kt
@@ -7,16 +7,26 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.workbuddy.databinding.ActivityMainBinding
 
-
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var activityListView: ListView? = null
+    private var arrayAdapter: ArrayAdapter<*>? = null
+    private lateinit var exampleActivities: Array<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityListView = findViewById(R.id.months_list);
+        exampleActivities = getResources().getStringArray(R.array.array_example_activities);
+        arrayAdapter = ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1, exampleActivities)
+        activityListView?.adapter = arrayAdapter
 
         val button = findViewById<Button>(R.id.StartMeeting)
         button.setOnClickListener {
