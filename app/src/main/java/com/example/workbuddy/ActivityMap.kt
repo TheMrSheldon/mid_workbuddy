81244468f51d5430a69590ba0e917f014cb5c244
Alexandro Steinert <delphi@vollbio.de>
2021-11-25 11:48:05 +0100

    Mapview with OSM

---
+++ app/src/main/java/com/example/workbuddy/ActivityMap.kt
@@ -0,0 +1,15 @@
+package com.example.workbuddy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.workbuddy.databinding.ActivityMainBinding
+
+class ActivityMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
--- app/src/main/java/com/example/workbuddy/MainActivity.kt
+++ app/src/main/java/com/example/workbuddy/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.example.workbuddy
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.workbuddy.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val button = findViewById<Button>(R.id.StartMeeting)
+        button.setOnClickListener {
+            openMapActivity()
+        }
+    }
 
-
-
+    fun openMapActivity() {
+        Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@MainActivity, ActivityMap::class.java)
+        startActivity(intent)
     }
 }
+
+
