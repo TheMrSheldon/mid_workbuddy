7d8bef5e1b3386a4c3b88fb4b7666e0fc6ecc1ce
Alexandro Steinert <delphi@vollbio.de>
2021-11-25 11:54:58 +0100

    Mapview return button

--- app/src/main/java/com/example/workbuddy/ActivityMap.kt
+++ app/src/main/java/com/example/workbuddy/ActivityMap.kt
@@ -3,6 +3,7 @@ package com.example.workbuddy
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.workbuddy.databinding.ActivityMainBinding
@@ -11,5 +12,15 @@ class ActivityMap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        val button = findViewById<ImageButton>(R.id.return_button)
+        button.setOnClickListener {
+            openMainActivity()
+        }
+    }
+
+    fun openMainActivity() {
+        Toast.makeText(this@ActivityMap, "You clicked me.", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@ActivityMap, MainActivity::class.java)
+        startActivity(intent)
     }
 }
