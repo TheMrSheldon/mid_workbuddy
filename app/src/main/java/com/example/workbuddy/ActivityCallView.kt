39ba043c450ac7a3f2aedd1188467d003422bf82
markossta <markos.stamatakis@yahoo.de>
2021-12-02 00:05:15 +0100

    changes related to audio input

--- app/src/main/java/com/example/workbuddy/ActivityCallView.kt
+++ app/src/main/java/com/example/workbuddy/ActivityCallView.kt
@@ -1,30 +1,104 @@
 package com.example.workbuddy
 
+import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import android.os.Environment
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import java.io.File
+import java.io.IOException
+import java.nio.file.Path
 
 class ActivityCallView : AppCompatActivity() {
+
+    private var recorder: MediaRecorder? = null
+    private var audiofile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_view)
-        val button = findViewById<MaterialButton>(R.id.end_call_button)
-        button.setOnClickListener {
+
+        val sessionName = intent.getStringExtra("session")
+
+        val cancel = findViewById<MaterialButton>(R.id.end_call_button)
+        val mute = findViewById<MaterialButton>(R.id.mute_button)
+
+        // define function for buttons
+        cancel.setOnClickListener {
             openMapActivity()
+        }
+
+        mute.setOnClickListener {
+            toggleAudioInput()
+        }
+
+        if (sessionName != null) {
+            startAudioInstance(sessionName)
+        } else { // TODO reimplement for better error handling
+           openMainActivity()
         }
     }
 
     fun openMainActivity() {
         val intent = Intent(this@ActivityCallView, MainActivity::class.java)
+        terminateAudioInstance()
         startActivity(intent)
     }
+
     fun openMapActivity() {
         Toast.makeText(this@ActivityCallView, "Call closed", Toast.LENGTH_SHORT).show()
         val intent = Intent(this@ActivityCallView, ActivityMap::class.java)
+        terminateAudioInstance()
         startActivity(intent)
+    }
+
+    fun startAudioInstance(sessionName: String) {
+        // create file
+        /** File dir = getExternalMediaDirs()
+        try {
+            audiofile = File.createTempFile(sessionName, ".3gp", dir)
+        } catch (e: IOException) {
+            Log.e("Audiofile", "storage error");
+        }
+
+        // create mediarecorder and start
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(audiofile.getAbsolutePath())
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("MediaRecorderObject", "prepare() failed")
+            }
+            start()
+        } **/
+    }
+
+
+    fun terminateAudioInstance() {
+        with(recorder) {
+            this?.stop()
+            this?.reset()
+            this?.release()
+        }
+
+    }
+
+    fun toggleAudioInput() {
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        audioManager.isMicrophoneMute = !audioManager.isMicrophoneMute
+
+        val toast = Toast.makeText(applicationContext, audioManager.isMicrophoneMute.toString(), Toast.LENGTH_SHORT)
+        toast.show()
+
     }
 }
--- app/src/main/java/com/example/workbuddy/MainActivity.kt
+++ app/src/main/java/com/example/workbuddy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.workbuddy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var exampleActivities: Array<String>
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,21 +28,39 @@ class MainActivity : AppCompatActivity() {
         activityListView = findViewById(R.id.months_list);
         exampleActivities = getResources().getStringArray(R.array.array_example_activities);
         arrayAdapter = ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1, exampleActivities)
-        activityListView?.adapter = arrayAdapter
 
+        // create session name for new session
+        var newSession = "session1"
+        if ((arrayAdapter as ArrayAdapter<*>).count != 0) {
+            val sessionName = (arrayAdapter as ArrayAdapter<*>).getItem(
+                (arrayAdapter as ArrayAdapter<*>).count - 1)
+            newSession = getSessionName(sessionName as String)
+        }
+        activityListView?.adapter = arrayAdapter
+        // make session runable
         val button = findViewById<Button>(R.id.StartMeeting)
         button.setOnClickListener {
-            openCallViewActivity()
+            openCallViewActivity(newSession)
         }
+
+        // TODO make old sessions viewable
+    }
+
+    fun getSessionName(lastSession: String): String {
+        val num = lastSession.replace("[^0-9]".toRegex(), "")
+        return  "session" + Integer.parseInt(num) + 1
+
     }
 
     fun openMapActivity() {
         val intent = Intent(this@MainActivity, ActivityMap::class.java)
         startActivity(intent)
     }
-    fun openCallViewActivity() {
+
+    fun openCallViewActivity(sessionNumber: String) {
         Toast.makeText(this@MainActivity, "Call started", Toast.LENGTH_SHORT).show()
         val intent = Intent(this@MainActivity, ActivityCallView::class.java)
+        intent.putExtra("session", sessionNumber)
         startActivity(intent)
     }
 }
