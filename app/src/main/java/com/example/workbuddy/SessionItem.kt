86a5d78a5efe8b0122b6dd2f5642dd0679a2a95a
Lennart Möller <kontakt@lennartmoeller.com>
2021-12-07 23:45:55 +0100

    Removed minor bug

--- app/src/main/java/com/example/workbuddy/SessionItem.kt
+++ app/src/main/java/com/example/workbuddy/SessionItem.kt
@@ -4,12 +4,15 @@ import android.text.format.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-class SessionItem(var filename: String) {
+class SessionItem(filename: String) {
 
-    var name: String = filename.split("_")[0]
-    var datetime: String
+    lateinit var filename: String
+    lateinit var name: String
+    lateinit var datetime: String
 
     init {
+        this.filename = filename
+        this.name = filename.split("_")[0]
         val timestamp: String = filename.split("_")[1]
         val date: Date = SimpleDateFormat("yyyyMMddHHmmss").parse(timestamp)
         this.datetime = DateFormat.format("dd.MM.yy, HH:mm", date).toString()
